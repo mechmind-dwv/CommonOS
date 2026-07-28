0efef2bf38b22eabc995317af3108174767dfa9d
mechmin-dwv <ia.mechmind@gmail.com>
2026-07-28 12:04:44 +0200

    feat(core): bootstrap CommonOS core package

---
+++ packages/common/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * CommonOS Core
+ *
+ * Foundation package.
+ */
+
+export const COMMONOS_VERSION = "0.1.0";
+
+export function helloCommonOS(): string {
+  return `CommonOS ${COMMONOS_VERSION}`;
+}
